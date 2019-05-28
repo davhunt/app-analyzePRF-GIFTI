bc7c748b7db4abf45053a847bb145951a69ecaa2
David Hunt <davhunt@indiana.edu>
2019-05-28 17:52:55 -0400

    naming conflict

--- compile.sh
+++ compile.sh
@@ -8,15 +8,15 @@ echo "/N/u/brlife/git/jsonlab" >> $log
 (cd /N/u/brlife/git/jsonlab && git log -1) >> $log
 echo "/N/u/brlife/git/mrTools" >> $log
 (cd /N/u/brlife/git/mrTools && git log -1) >> $log
-echo "/N/u/brlife/git/vistasoft" >> $log
-(cd /N/u/brlife/git/vistasoft && git log -1) >> $log
+#echo "/N/u/brlife/git/vistasoft" >> $log
+#(cd /N/u/brlife/git/vistasoft && git log -1) >> $log
 echo "/N/u/davhunt/Carbonate/analyzePRF/utilities" >> $log
 (cd /N/u/davhunt/Carbonate/analyzePRF/utilities && git log -1) >> $log
 
 mkdir -p compiled
 
 cat > build.m <<END
-addpath(genpath('/N/u/brlife/git/vistasoft'))
+%addpath(genpath('/N/u/brlife/git/vistasoft'))
 addpath(genpath('/N/u/brlife/git/jsonlab'))
 addpath(genpath('/N/soft/mason/SPM/spm8'))
 addpath(genpath('/N/u/davhunt/Carbonate/analyzePRF/utilities'))
