97e97e71c01b0324ce0d344e48224964c0d2a29a
David Hunt <davhunt@indiana.edu>
2019-05-28 17:52:55 -0400

    naming conflict

--- build.m
+++ build.m
@@ -1,4 +1,4 @@
-addpath(genpath('/N/u/brlife/git/vistasoft'))
+%addpath(genpath('/N/u/brlife/git/vistasoft'))
 addpath(genpath('/N/u/brlife/git/jsonlab'))
 addpath(genpath('/N/soft/mason/SPM/spm8'))
 addpath(genpath('/N/u/davhunt/Carbonate/analyzePRF/utilities'))
--- main.m
+++ main.m
@@ -4,7 +4,7 @@ if ~isdeployed
 	disp('loading paths for IUHPC')
 	addpath(genpath('/N/u/brlife/git/jsonlab'))
 	addpath(genpath('/N/u/brlife/git/mrTools'))
-	addpath(genpath('/N/u/brlife/git/vistasoft'))
+	%addpath(genpath('/N/u/brlife/git/vistasoft'))
 	addpath(genpath('/N/u/davhunt/Carbonate/analyzePRF/utilities'))
 end
 
