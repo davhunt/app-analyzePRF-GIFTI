4cece341dabc8730bce24897b46e3e7fb3f722ca
David Hunt <davhunt@indiana.edu>
2019-05-28 18:07:37 -0400

    naming conflict

--- compile.sh
+++ compile.sh
@@ -18,7 +18,7 @@ mkdir -p compiled
 cat > build.m <<END
 %addpath(genpath('/N/u/brlife/git/vistasoft'))
 addpath(genpath('/N/u/brlife/git/jsonlab'))
-addpath(genpath('/N/soft/mason/SPM/spm8'))
+%addpath(genpath('/N/soft/mason/SPM/spm8'))
 addpath(genpath('/N/u/davhunt/Carbonate/analyzePRF/utilities'))
 mcc -m -R -nodisplay -a /N/u/brlife/git/vistasoft/mrDiffusion/templates -d compiled main
 exit
