6444e181e2b64f25ba4b539ffee6bd81dd82dc0f
David Hunt <davhunt@indiana.edu>
2019-05-27 22:11:41 -0400

    first commit

---
+++ main.m
@@ -0,0 +1,17 @@
+function [] = main()
+
+if ~isdeployed
+	disp('loading paths for IUHPC')
+	addpath(genpath('/N/u/brlife/git/jsonlab'))
+	addpath(genpath('/N/u/brlife/git/mrTools'))
+	addpath(genpath('/N/u/brlife/git/vistasoft'))
+	addpath(genpath('/N/u/davhunt/Carbonate/analyzePRF/utilities'))
+end
+
+% load my own config.json
+config = loadjson('config.json');
+
+% compute pRF
+analysis_HCP(config.gifti_dir);
+
+end
