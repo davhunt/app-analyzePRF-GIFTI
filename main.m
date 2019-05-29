00eeb776dbf283e3a7adb1a01f6f3b88298b1d7a
David Hunt <davhunt@indiana.edu>
2019-05-29 00:22:22 -0400

    asdf

--- main.m
+++ main.m
@@ -12,6 +12,13 @@ end
 % load my own config.json
 config = loadjson('config.json');
 
+disp('asdf')
+PATH = getenv('PATH');
+setenv('PATH', [PATH ':/usr/bin']);
+LD_LIBRARY_PATH = getenv('LD_LIBRARY_PATH');
+setenv('LD_LIBRARY_PATH', [LD_LIBRARY_PATH ':/N/u/davhunt/Carbonate/app-analyzePRF-GIFTI/workbench/libs_rh_linux64']);
+disp('asdff')
+
 % compute pRF
 analysis_HCP(config.gifti_dir);
 
