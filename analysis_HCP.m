d7eda6ca9ff947adcb5ebca342cdd39e5d435490
David Hunt <davhunt@indiana.edu>
2019-06-06 14:51:03 -0400

    libraries

--- analysis_HCP.m
+++ analysis_HCP.m
@@ -85,9 +85,10 @@ if ~isdeployed
 end
 data = {};
 
-PATH = getenv('PATH');
-setenv('PATH', [PATH ':/usr/bin']);
-%LD_LIBRARY_PATH = getenv('LD_LIBRARY_PATH');
+%PATH = getenv('PATH');
+%setenv('PATH', [PATH ':/usr/bin']);
+LD_LIBRARY_PATH = getenv('LD_LIBRARY_PATH');
+disp(LD_LIBRARY_PATH);
 %setenv('LD_LIBRARY_PATH', [LD_LIBRARY_PATH ':/N/u/davhunt/Carbonate/app-analyzePRF-GIFTI/workbench/libs_rh_linux64']);
 
 for p=1:6
