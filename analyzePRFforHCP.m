4ca7b89a359fd49744bcd3030106addd1f947873
David Hunt <davhunt@indiana.edu>
2020-03-02 15:37:29 -0500

    process non-HCP stimuli/apertures

--- analyzePRFforHCP.m
+++ analyzePRFforHCP.m
@@ -37,10 +37,14 @@ clear a1;
 data = {};
 LD_LIBRARY_PATH = getenv('LD_LIBRARY_PATH');
 
-for p=1:6
+sumTR = 1;
+for p=1:size(stimulus,2)
 %  data{p} = double(getfield(ciftiopen([subjs{wh}(1:end-2) '/' runs{p}],wbcmd),'cdata'));
 %  data{p} = data{p}(1:59412,:);
-  data{p} = double(cat(1, func_L_gii.cdata(:,p*300-299:p*300), func_R_gii.cdata(:,p*300-299:p*300)));
+%  data{p} = double(cat(1, func_L_gii.cdata(:,p*300-299:p*300), func_R_gii.cdata(:,p*300-299:p*300)));
+
+  data{p} = double(cat(1, func_L_gii.cdata(:,sumTR:sumTR+size(stimulus{p},3)-1), func_R_gii.cdata(:,sumTR:sumTR+size(stimulus{p},3)-1)));
+  sumTR = sumTR + size(stimulus{p},3);
 end
 
 % deal with subsetting
@@ -54,7 +58,7 @@ case 3
   data =     cellfun(@(x) x(:,151:300),  data,    'UniformOutput',0);
 end
 % fit the models
-a1 = analyzePRF(stimulus,data,tr,struct('seedmode',[0 1 2]));
+a1 = analyzePRF(stimulus,data,tr,struct('seedmode',[-2]));
 
 % convert pAngle results to standard pRF convention (0-180 deg = UVM -> L/R HM -> LVM)
 for i = 1:size(a1.ang)
@@ -80,6 +84,12 @@ end
 
 % prepare outputs
 quants = {'ang' 'ecc' 'gain' 'meanvol' 'R2' 'rfsize'};
+
+totalVerticesLH = size(func_L_gii.cdata,1); totalVerticesRH = size(func_R_gii.cdata,1);
+totalVertices = totalVerticesLH + totalVerticesRH;
+allresults = zeros(totalVertices,length(quants),1,1,'single');
+
+
 %allresults = zeros(91282,length(quants),length(subjs),3,'single');
 % 91282 grayordinates x 6 quants x 184 subjects x 3 model fits
 allresults = zeros(59412,length(quants),1,1,'single');
@@ -96,26 +106,27 @@ allresults(:,6,wh,typ) = a1.rfsize*pxtodeg; % convert to degrees
 allresults = squish(permute(allresults,[1 3 4 2]),1);
 allresults(allresults(:,2)==0,1) = NaN;
 %allresults = permute(reshape(allresults,[91282 184 3 6]),[1 4 2 3]);
-allresults = permute(reshape(allresults,[59412 1 1 6]),[1 4 2 3]);
+%allresults = permute(reshape(allresults,[59412 1 1 6]),[1 4 2 3]);
+allresults = permute(reshape(allresults,[totalVertices 1 1 6]),[1 4 2 3]);
 
 % first 29696 grayordinates in CIFTI are lh, next 29716 are rh
-lh_polarAngle = allresults(1:29696,1,1,1);
-rh_polarAngle = allresults(29697:59412,1,1,1);
+lh_polarAngle = allresults(1:totalVerticesLH,1,1,1);
+rh_polarAngle = allresults(totalVerticesLH+1:totalVertices,1,1,1);
 
-lh_eccentricity = allresults(1:29696,2,1,1);
-rh_eccentricity = allresults(29697:59412,2,1,1);
+lh_eccentricity = allresults(1:totalVerticesLH,2,1,1);
+rh_eccentricity = allresults(totalVerticesLH+1:totalVertices,2,1,1);
 
-lh_gain = allresults(1:29696,3,1,1);
-rh_gain = allresults(29697:59412,3,1,1);
+lh_gain = allresults(1:totalVerticesLH,3,1,1);
+rh_gain = allresults(totalVerticesLH+1:totalVertices,3,1,1);
 
-lh_meanvol = allresults(1:29696,4,1,1);
-rh_meanvol = allresults(29697:59412,4,1,1);
+lh_meanvol = allresults(1:totalVerticesLH,4,1,1);
+rh_meanvol = allresults(totalVerticesLH+1:totalVertices,4,1,1);
 
-lh_r2 = allresults(1:29696,5,1,1);
-rh_r2 = allresults(29697:59412,5,1,1);
+lh_r2 = allresults(1:totalVerticesLH,5,1,1);
+rh_r2 = allresults(totalVerticesLH+1:totalVertices,5,1,1);
 
-lh_rfWidth = allresults(1:29696,6,1,1);
-rh_rfWidth = allresults(29697:59412,6,1,1);
+lh_rfWidth = allresults(1:totalVerticesLH,6,1,1);
+rh_rfWidth = allresults(totalVerticesLH+1:totalVertices,6,1,1);
 
 save('prf_results.mat', 'lh_polarAngle', 'rh_polarAngle', 'lh_eccentricity', 'rh_eccentricity', ...
   'lh_gain', 'rh_gain', 'lh_meanvol', 'rh_meanvol', 'lh_r2', 'rh_r2', 'lh_rfWidth', 'rh_rfWidth');
