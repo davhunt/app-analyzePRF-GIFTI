6c525cf164d4284e16716a40ea0b16d5d7961f33
David Hunt <davhunt@indiana.edu>
2020-09-02 12:32:42 -0400

    include sphere.reg in surfaces in case they're needed

--- create_vtks.sh
+++ create_vtks.sh
@@ -9,6 +9,8 @@ mris_convert prf/surfaces/lh.pial.gii prf/surfaces/lh.pial.vtk
 mris_convert prf/surfaces/rh.pial.gii prf/surfaces/rh.pial.vtk
 mris_convert prf/surfaces/lh.sphere.gii prf/surfaces/lh.sphere.vtk
 mris_convert prf/surfaces/rh.sphere.gii prf/surfaces/rh.sphere.vtk
+mris_convert prf/surfaces/lh.sphere.reg.gii prf/surfaces/lh.sphere.reg.vtk
+mris_convert prf/surfaces/rh.sphere.reg.gii prf/surfaces/rh.sphere.reg.vtk
 mris_convert prf/surfaces/lh.inflated.gii prf/surfaces/lh.inflated.vtk
 mris_convert prf/surfaces/rh.inflated.gii prf/surfaces/rh.inflated.vtk
 
