b64a6cb74eba5fbbe03151d21b4300a7dc9413ab
David Hunt <davhunt@indiana.edu>
2020-09-28 11:53:35 -0400

    update paths for matlab

--- compile.sh
+++ compile.sh
@@ -8,19 +8,20 @@ echo "/N/u/brlife/git/jsonlab" >> $log
 (cd /N/u/brlife/git/jsonlab && git log -1) >> $log
 echo "/N/u/brlife/git/mrTools" >> $log
 (cd /N/u/brlife/git/mrTools && git log -1) >> $log
-echo "/N/u/davhunt/Carbonate/analyzePRF/utilities" >> $log
-(cd /N/u/davhunt/Carbonate/analyzePRF/utilities && git log -1) >> $log
+echo "/N/u/brlife/git/NIfTI" >> $log
+(cd /N/u/brlife/git/NIfTI && git log -1) >> $log
+echo "utilities" >> $log
+(cd utilities && git log -1) >> $log
 echo "/N/u/davhunt/Carbonate/Downloads/gifti-1.8" >> $log
 (cd /N/u/davhunt/Carbonate/Downloads/gifti-1.8 && git log -1) >> $log
-echo "/N/u/davhunt/Carbonate/NIfTI_cifti_matlab_tools" >> $log
-(cd /N/u/davhunt/Carbonate/NIfTI_cifti_matlab_tools && git log -1) >> $log
 
 cat > build.m <<END
 addpath(genpath('/N/u/brlife/git/jsonlab'))
 addpath(genpath('/N/u/brlife/git/mrTools'))
-addpath(genpath('/N/u/davhunt/Carbonate/analyzePRF/utilities'))
+addpath(genpath('/N/u/brlife/git/NIfTI'))
+addpath(genpath('utilities'))
 addpath(genpath('/N/u/davhunt/Carbonate/Downloads/gifti-1.8'))
-addpath(genpath('/N/u/davhunt/Carbonate/NIfTI_cifti_matlab_tools'))
+
 mcc -m -R -nodisplay -a /N/u/brlife/git/vistasoft/mrDiffusion/templates -d compiled main
 exit
 END
--- main.m
+++ main.m
@@ -4,9 +4,9 @@ if ~isdeployed
 	disp('loading paths for IUHPC')
 	addpath(genpath('/N/u/brlife/git/jsonlab'))
 	addpath(genpath('/N/u/brlife/git/mrTools'))
-	addpath(genpath('/N/u/davhunt/Carbonate/analyzePRF/utilities'))
+        addpath(genpath('/N/u/brlife/git/NIfTI'))
+	addpath(genpath('utilities'))
         addpath(genpath('/N/u/davhunt/Carbonate/Downloads/gifti-1.8/'))
-        addpath(genpath('/N/u/davhunt/Carbonate/NIfTI_cifti_matlab_tools'))
 end
 
 
