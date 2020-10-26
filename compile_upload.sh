357372091e387179e65e43e41067160712a72ce3
David Hunt <davhunt@indiana.edu>
2020-10-26 16:25:11 -0400

    make local libraries

--- compile_upload.sh
+++ compile_upload.sh
@@ -10,18 +10,15 @@ log=compiled_upload/commit_ids.txt
 true > $log
 echo "/N/u/brlife/git/jsonlab" >> $log
 (cd /N/u/brlife/git/jsonlab && git log -1) >> $log
-echo "/N/u/brlife/git/mrTools" >> $log
-(cd /N/u/brlife/git/mrTools && git log -1) >> $log
 echo "utilities" >> $log
-(cd /N/u/davhunt/Carbonate/analyzePRF/utilities && git log -1) >> $log
+(cd utilities && git log -1) >> $log
 echo "gifti-1.8" >> $log
-(cd /N/u/davhunt/Carbonate/Downloads/gifti-1.8 && git log -1) >> $log
+(cd gifti-1.8 && git log -1) >> $log
 echo "NIfTI_cifti_matlab_tools" >> $log
-(cd /N/u/davhunt/Carbonate/NIfTI_cifti_matlab_tools && git log -1) >> $log
+(cd NIfTI_cifti_matlab_tools && git log -1) >> $log
 
 cat > build.m <<END
 addpath(genpath('/N/u/brlife/git/jsonlab'))
-addpath(genpath('/N/u/brlife/git/mrTools'))
 addpath(genpath('utilities'))
 addpath(genpath('gifti-1.8'))
 addpath(genpath('NIfTI_cifti_matlab_tools'))
